23c9330b1619937e48b839d5e53c6b5b0ecf8065
Rakib <rakibkhan@live.co.uk>
2024-08-05 15:16:20 +0100

    first commit

---
+++ pages/signup.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Signup() {
+  return (
+    <div>
+      Signup
+      <Link href="/">go back</Link>
+    </div>
+  );
+}
