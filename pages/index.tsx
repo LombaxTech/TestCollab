38cd5cb1bcfaff04e775d7f3ecbfa2e59b81bceb
Rakib <rakibkhan@live.co.uk>
2024-08-05 15:37:34 +0100

    added a message by rakib

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 
 export default function Home() {
-  return <div>Home
-    <p>hello from farid</p>
-  </div>;
-  
+  return (
+    <div>
+      Home
+      <p>hello from farid</p>
+      <h1>Hello from Rakib</h1>
+    </div>
+  );
 }
