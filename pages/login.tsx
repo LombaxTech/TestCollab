9d82e971bd655663124786b0c40fb6e820daf613
Rakib <rakibkhan@live.co.uk>
2024-08-06 15:40:06 +0100

    added button to login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,13 +1,10 @@
 const Login = () => {
-    return ( 
-        <>
-            <p>Login page</p>
-            <p>Login page 2</p>
+  return (
+    <div className="">
+      <h1>login page</h1>
+      <button className="bg-red-500 p-2">Login</button>
+    </div>
+  );
+};
 
-        </>
-
-     );
-}
- 
 export default Login;
-
